ab0226efbf77c90728cd3fba84c7b42635b1be66
ikeda <ikeda@posl.ait.kyushu-u.ac.jp>
2019-11-05 15:07:29 +0900

    mongoDBのワーニング解消

--- app/server.js
+++ app/server.js
@@ -1,16 +1,20 @@
-let mongoose = require('mongoose')
-let app = require('./app.js')
+const mongoose = require('mongoose')
+const app = require('./app.js')
 
 // mongoDB接続
 mongoose.Promise = global.Promise;
-mongoose.connect('mongodb://localhost:27017/ExpressAPI', { useNewUrlParser: true });
+mongoose.connect('mongodb://localhost:27017/ExpressAPI',
+    {
+        useNewUrlParser: true,
+        useUnifiedTopology: true
+    });
 mongoose.connection.on('error', function (err) {
     console.error('MongoDB connection error: ' + err);
     process.exit(-1);
 });
 
 //サーバ起動
-let port = process.env.PORT || 3000; // port番号を指定
+const port = process.env.PORT || 3000; // port番号を指定
 app.listen(port);
 console.log('listen on port ' + port);
 // エラーの詳細表示

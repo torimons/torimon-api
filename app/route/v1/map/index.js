f63641882fd464b124ba226cd2deef30ec99b286
ikeda <ikeda@posl.ait.kyushu-u.ac.jp>
2019-11-11 15:04:36 +0900

    POST /map/で総合学習プラザのテストデータを入力するテストapi追加

--- app/route/v1/map/index.js
+++ app/route/v1/map/index.js
@@ -5,6 +5,7 @@ const router = express.Router();
 // map用のルーティング
 router.use('/', require('./map-api-root.js'));
 router.use('/', require('./map-api-id.js'));
+router.use('/', require('./test-post.js'))
 
 //routerをモジュールとして扱う準備
 module.exports = router;

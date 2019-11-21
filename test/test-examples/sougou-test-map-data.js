d7039cdf933257eea2d61a4e3e26507ec40bf64d
ikeda <ikeda@posl.ait.kyushu-u.ac.jp>
2019-11-21 15:28:16 +0900

    総合学習プラザのテストデータを1階と2階で分離

--- test/test-examples/sougou-test-map-data.js
+++ test/test-examples/sougou-test-map-data.js
@@ -3,16 +3,15 @@ const sougou =
     "maps": [
         {
             "id": 0,
-            "name": "SougouGakusyuPlaza",
+            "name": "SougouGakusyuPlaza_1F",
             "spot": [
                 {
                     "id": 0,
-                    "name": 101,
+                    "name": "101",
                     "coordinate": {
                         "lat": 33.5954558,
                         "lng": 130.2179447
                     },
-                    "floor": 1,
                     "shape": {
                         "type": "Polygon",
                         "coordinates": [
@@ -22,27 +21,7 @@ const sougou =
                         ]
                     },
                     "gate_node_ids": [0],
-                },
-                {
-                    "id": 10,
-                    "name": 201,
-                    "coordinate": {
-                        "lat": 33.5954558,
-                        "lng": 130.2179447
-                    },
-                    "floor": 2,
-                    "shape": {
-                        "type": "Polygon",
-                        "coordinates": [
-                            [
-                                [130.217816, 33.595257], [130.217783, 33.595517], [130.217915, 33.595558], [130.217942, 33.595495]
-                            ]
-                        ]
-                    },
-                    "gate_node_ids": [10],
-                    "detail_map_ids": [],
-                    "others": {}
-                },
+                }
             ],
             "node": [
                 {
@@ -53,17 +32,6 @@ const sougou =
                         "lat": 33.5954558,
                         "lng": 130.2179447
                     },
-                    "floor": 1
-                },
-                {
-                    "id": 10,
-                    "map_id": 0,
-                    "spot_id": 10,
-                    "coordinate": {
-                        "lat": 33.5954558,
-                        "lng": 130.2179447
-                    },
-                    "floor": 2
                 },
             ],
             "edge": [
@@ -86,7 +54,45 @@ const sougou =
                     "lng": 130.2177802
                 }
             },
-        }
+        },
+        {
+            "id": 1,
+            "name": "SougouGakusyuPlaza_2F",
+            "spot": [
+                {
+                    "id": 10,
+                    "name": "201",
+                    "coordinate": {
+                        "lat": 33.5954558,
+                        "lng": 130.2179447
+                    },
+                    "shape": {
+                        "type": "Polygon",
+                        "coordinates": [
+                            [
+                                [130.217816, 33.595257], [130.217783, 33.595517], [130.217915, 33.595558], [130.217942, 33.595495]
+                            ]
+                        ]
+                    },
+                    "gate_node_ids": [10],
+                    "detail_map_ids": [],
+                    "others": {}
+                }
+            ],
+            "node": [
+                {
+                    "id": 10,
+                    "map_id": 0,
+                    "spot_id": 10,
+                    "coordinate": {
+                        "lat": 33.5954558,
+                        "lng": 130.2179447
+                    },
+                }
+            ],
+            "edge": []
+        },
     ]
 }
+ 
 module.exports = sougou;
